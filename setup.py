494e665319d1d7812d8d71c9aa365af880b8995b
Fabien Marteau <fabien.marteau@armadeus.com>
2021-06-16 13:58:26 +0200

    change name according to official documentation

--- setup.py
+++ setup.py
@@ -11,14 +11,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="cocotbext.spi",
+    name="cocotbext-spi",
+    version="0.1",
     use_scm_version={
         "relative_to": __file__,
         "write_to": "cocotbext/spi/version.py",
     },
     author="Fabien Marteau",
     author_email="mail@fabienm.eu",
-    description="Cocotb SPI module",
+    description="CocoTB SPI module",
     long_description=long_description,
     url="https://github.com/Martoni/cocotbext-spi.git",
     packages=["cocotbext.spi"],
